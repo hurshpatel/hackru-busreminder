1e1fdabbce02cdc95fc232c3ac7d60f51c80e149
Julius Parishy <julius@fitocracy.com>
2013-10-12 19:39:08 -0400

    refactor

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ var twilio = require('twilio');
 var app = express();
 var twilio = new twilio.RestClient("AC2c3b364a9ee85a3422bbe0e6603c8588", "6a0cf57047fe549c324924108b5e4c6b");
 
-app.get("/", function(request, response) {
-
-  response.send("Bus Reminder")
-});
-
 var respond = function(response, data) {
 
   var fullData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + data;
@@ -17,9 +12,19 @@ var respond = function(response, data) {
   response.end();
 };
 
+var textBack(response, message) {
+
+  respond(response, "<Response>\n<Message>" + message + "</Message>\n</Response>");
+};
+
+app.get("/", function(request, response) {
+
+  response.send("Bus Reminder")
+});
+
 app.post("/receive", function(request, response) {
 
-  respond(response, "<Response>\n<Message>Sup, yo</Message>\n</Response>");
+  textBack("request: " + request.body);
 });
 
 var port = process.env.PORT || 5000;
