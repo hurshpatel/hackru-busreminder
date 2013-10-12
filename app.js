ea3fa3ea3bdf14fc2cbb9e448b30f65e25b352cd
Julius Parishy <julius@fitocracy.com>
2013-10-12 18:38:41 -0400

    add receive hook

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get("/", function(request, response) {
   response.send("Bus Reminder")
 });
 
+app.post("/receive", function(request, response) {
+
+  response.send("ok");
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 
