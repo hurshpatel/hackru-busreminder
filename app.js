452686f0cf2a8664e074c0ae7f3f2eb63b52d810
Julius Parishy <julius@fitocracy.com>
2013-10-12 18:27:41 -0400

    clean up github tutorial

--- app.js
+++ app.js
@@ -6,16 +6,6 @@ app.get("/", function(request, response) {
   response.send("Bus Reminder")
 });
 
-app.get("/foo", function(request, response) {
-
-  response.send("jkhjk")
-});
-
-app.get("/abc", function(request, response) {
-
-  response.send("5353")
-});
-
 app.listen(8770);
 
 console.log("Server started (8770)");""
