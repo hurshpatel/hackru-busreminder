39b93024b4db6515089392ee5885ada8a6f3eb1f
Alexander Najem <AlexNaj88@gmail.com>
2013-10-12 18:20:15 -0400

    copy and paste reqfunc

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.get("/foo", function(request, response) {
   response.send("jkhjk")
 });
 
+app.get("/abc", function(request, response) {
+
+  response.send("5353")
+});
+
 app.listen(8770);
 
 console.log("Server started (8770)");""
