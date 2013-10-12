7caf45ceb8126ce9d74848dd7ad0adcb7b00981d
hurshpatel <Hursh@nbp-116-47.nbp.ruw.rutgers.edu>
2013-10-12 18:10:43 -0400

    copy and pasted app.get
    changed "/" to "/foo" and "Bus Reminder" to "jkhjk"

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get("/", function(request, response) {
   response.send("Bus Reminder")
 });
 
+app.get("/foo", function(request, response) {
+
+  response.send("jkhjk")
+});
+
 app.listen(8770);
 
 console.log("Server started (8770)");""
