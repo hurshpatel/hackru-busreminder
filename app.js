c2030612905e8840168d06ff51723cca54e06636
Julius Parishy <julius@fitocracy.com>
2013-10-12 18:47:11 -0400

    update twilio

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var twilio = require('twilio');
+
 var app = express();
+var twilio = new Twilio.RestClient("AC2c3b364a9ee85a3422bbe0e6603c8588", "6a0cf57047fe549c324924108b5e4c6b");
 
 app.get("/", function(request, response) {
 
@@ -8,6 +11,7 @@ app.get("/", function(request, response) {
 
 app.post("/receive", function(request, response) {
 
+  console.log(request);
   response.send("ok");
 });
 
